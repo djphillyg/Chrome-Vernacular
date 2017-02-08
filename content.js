0833c531254b254a94a1cce17c5ef7caffe51190
goldbepa <P7n16l4d7g45>
2017-02-07 22:04:27 -0600

    first commit

---
+++ content.js
@@ -0,0 +1,21 @@
+
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            
+            var replacedText = text.replace(/(k$|c$)/,'cc');
+
+            if (replacedText !== text) {
+                console.log('replaced');
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
